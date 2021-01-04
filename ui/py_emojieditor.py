b06a4b4f5d92ea4319f33c14be64db1137d9e037
Sophie 'Fynne' Schumann <me@fynne.eu>
2021-01-04 18:44:34 +0100

    emojis are now cached

--- ui/py_emojieditor.py
+++ ui/py_emojieditor.py
@@ -1,9 +1,13 @@
+import os
 from lib.matrix import MatrixAPI
 from .emojieditor import Ui_EmojiEditor
 from PyQt5.QtWidgets import QDialog, QLabel, QPlainTextEdit
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import Qt
-from typing import List, Dict
+from typing import List, Dict, Optional
+from lib.matrix import MXC_RE
+
+EMOJI_DIR = r'emojis'
 
 
 class EmojiEditor(Ui_EmojiEditor, QDialog):
@@ -26,13 +30,36 @@ class EmojiEditor(Ui_EmojiEditor, QDialog):
             g.addWidget(QPlainTextEdit(v['url'], parent=self), row, 1)
             
             pm = QPixmap()
-            emojiBytes = self.matrix.media_get_thumbnail(v['url'], width=128, height=128)
+            emojiBytes = self.getCachedEmoji(v['url'], width=128, height=128)
             pm.loadFromData(emojiBytes)
-            pm.scaled(128, 128, Qt.KeepAspectRatio)
+            pm = pm.scaled(128, 128, Qt.KeepAspectRatio)
             preview = QLabel(self)
             preview.setPixmap(pm)
             g.addWidget(preview, row, 2)
             
             row +=1
+
+
+    def getCachedEmoji(self, mxcurl, width: int, height: int) -> Optional[bytes]:
+        p = os.path
+        if not p.lexists(EMOJI_DIR) and not p.isdir(EMOJI_DIR):
+            os.mkdir(EMOJI_DIR)
+
+        m = MXC_RE.search(mxcurl)
+        if m is None:
+            raise Exception("MXC url could not be parsed")
+        if len(m.groups()) == 2:
+            servername, mediaid = m.groups()
+            mediapath = p.join(EMOJI_DIR, mediaid + ".bin")
+            if p.lexists(mediapath):
+                with open(mediapath, 'rb') as f:
+                    return f.read()
+
+            emojiBytes = self.matrix.media_get_thumbnail(mxcurl, width=width, height=height)
+            with open(mediapath, 'wb') as f:
+                f.write(emojiBytes)
+            return emojiBytes
+
+        
 
     
